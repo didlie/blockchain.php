6979a3a6d09713f4bd72c5933829d8ea975a3a96
didlie <everforward@live.com>
2024-07-25 00:41:43 +0400

    Changes to be committed: 	modified:   blockchain.php  Untracked files: 	blockchain.file 	blocknavigation.htm 	blocknavigation.php 	filemap.php 	index.php 	smartContract.js

--- blockchain.php
+++ blockchain.php
@@ -8,12 +8,14 @@
 $GLOBALS['blockchainfile'] = "blockchain.file";
 $GLOBALS['filehandle'] = NULL;
 
-if(empty($_POST) || !isset($_POST['content']) || $_POST['content'] == ""){
-    showForm();
-}else{
-    $block = addToBlockChain();
-    displayNewBlock($block);
-}
+// if(empty($_POST) || !isset($_POST['content']) || $_POST['content'] == ""){
+//     // showForm();
+// }else{
+//     $block = addToBlockChain();
+//     displayNewBlock($block);
+// }
+// showForm();
+
 
 function showForm(){
     echo <<<form
@@ -28,10 +30,11 @@ function getPreviousHash(){
     if(!$GLOBALS['filehandle'] = fopen($GLOBALS['blockchainfile'],"a+b")) die("unable to get file handle.");
     flock($GLOBALS['filehandle'],LOCK_EX);
     if(is_file($GLOBALS['blockchainfile']) && filesize($GLOBALS['blockchainfile']) > 0){
-        do{
-            $block = fgets($GLOBALS['filehandle']);
-        }while(!feof($GLOBALS['filehandle']));
-        return hash("sha1",$block);
+        while(($block = fgets($GLOBALS['filehandle'])) !== false){
+            $lastBlock = $block;
+        }
+        // fclose($GLOBALS['filehandle']);
+        return hash("sha1",$lastBlock);
     }else{
         return hash("sha1","EMDM");
     }
@@ -47,15 +50,26 @@ function addToBlockChain(){
     return $block;
 }
 
+/*********************bells and whistles here! */
 function displayNewBlock($block){
     $a = json_decode($block);
     $a->content = decode($a->content);
     echo "<h3>A new block was added to the blockchain.</h3>";
     echo "<h5>Block details:</h5>";
     foreach($a as $k => $v){
-        echo "<pre>{$k} : {$v}</pre>";
+        $v = trim($v);
+        $echoTxt = "<pre>{$k} : <span id='{$k}'>{$v}</span></pre>";
+        $echoTxt .= ($k == "content") ? "<button id='evalThis'>RUN</button>":"";
+        echo($echoTxt);
     }
+    includeScript();
 }
+
+        function includeScript(){
+            echo "<script>";
+            echo file_get_contents("smartContract.js");
+            echo "</script>";
+        }
 
 function decode($string){
   return zlib_decode(hex2bin($string));
